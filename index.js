68a3ab379c07c3d7d33f5292b6a5c16e99fa6947
Andrei Komolov <andreikomolov@capgemini.com>
2021-09-14 17:32:06 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,49 @@
+import express from 'express'
+import path from 'path';
+import { requestTime, logger} from './middlewares.js'
+
+const __dirname = path.resolve()
+
+const PORT = process.env.PORT ?? 3000
+const app = express()
+
+console.log(PORT)
+console.log(__dirname)
+
+app.set('view engine', 'ejs')
+app.set('views', path.resolve(__dirname, 'ejs') )
+console.log(app.get('view engine'))
+
+
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(requestTime)
+app.use(logger)
+
+
+app.get('/', (reg, res) => {
+    res.render('index', {title: 'Main Page', active: 'main'})
+})
+
+app.get('/features', (reg, res) => {
+    res.render('features', {title: 'Features Page', active: 'features'})
+})
+
+
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
+// })
+
+// app.get('/features', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'static', 'features.html'))
+// })
+
+// app.get('/download', (req, res) => {
+//     console.log(req.requestTime)
+//     res.download(path.resolve(__dirname, 'static', 'index.html'))
+// })
+
+app.listen(PORT, () => {
+    console.log(`Server has been started on port ${PORT}...`)
+})
+
